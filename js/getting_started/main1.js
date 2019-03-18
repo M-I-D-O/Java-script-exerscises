bda14322a37871e035e41bf5691b286bb89dfab5
mido <ahmadalali1@live.com>
2019-03-18 15:07:39 +0100

    first commit

---
+++ js/getting_started/main1.js
@@ -0,0 +1,20 @@
+console.log("let's get started");
+console.log("Who I am  ?");
+
+let myName = "Mohammed";
+lastName = "Al ali";
+console.log(myName ,lastName);
+let  name = "Mido";
+city = "Berlin";
+job = "Web developer";
+age = "22";
+born ="Syria";
+console.log(name ,city ,job ,age ,born);
+let personName ="John Smith"
+personAge = "43 years old" 
+personJob = " Teacher"
+personCity = "Berlin "
+console.log(personName +" is a "+ personAge+ personJob+ " who lives in "+ personCity)
+
+
+
